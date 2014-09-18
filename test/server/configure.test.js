1fc0cb53027d0063037247cd2f593c31efa40594
Dan Crews <crewsd@gmail.com>
2014-09-18 16:23:35 -0600

    Updating configure tests

--- test/server/configure.test.js
+++ test/server/configure.test.js
@@ -1,8 +1,11 @@
 /*jshint expr: true */
 var chai = require('chai')
   , expect = require('chai').expect
-  , featureClient = require('../../lib')
-  , Q = require('q');
+  , featureClient = require('../../lib');
+
+
+process.env.FEATURE_URL = 'http://example.com/';
+process.env.FEATURE_DEVKEY = 'exp-dev-key-here';
 
 
 describe('.config interface:', function() {
@@ -13,14 +16,62 @@ describe('.config interface:', function() {
       }).to.throw(Error, 'Cannot register experiments without a config. Please see docs');
     });
   });
-  describe('Given a valid devKey and config', function() {
-    it('should attempt to call the Feature dashboard');
+  describe('Given no experiments array', function() {
+    it('should reject with an Error', function() {
+      expect(function() {
+        featureClient.configure({});
+      }).to.throw(Error, 'Cannot register experiments without `experiments`. Please see the docs');
+    });
   });
-  describe('Given no `name`,', function() {
-    it('should assign "noName" as the name');
+  describe('Given no experiments in array', function() {
+    it('should reject with an Error', function() {
+      expect(function() {
+        featureClient.configure({
+          experiments : []
+        });
+      }).to.throw(Error, 'Cannot register experiments without `experiments`. Please see the docs');
+    });
   });
-  describe('Given a no `devKey`,', function() {
-    it('should recognize the config object');
+  describe('Given no devKey / featureUrl,', function() {
+    it('should fallback to `process.env`', function() {
+      var feature = featureClient.configure({
+        experiments: ['testEx']
+      });
+
+      expect(feature.featureUrl).to.equal('http://example.com/');
+      expect(feature.devKey).to.equal('exp-dev-key-here');
+    });
   });
+  describe('Given a passed devKey / featureUrl,', function() {
+    it('should not fallback to `process.env`', function() {
+      var feature = featureClient.configure({
+        featureUrl: 'https://example.com/',
+        devKey: 'new-dev-key-here',
+        experiments: ['testEx2']
+      });
+
+      expect(feature.featureUrl).to.equal('https://example.com/');
+      expect(feature.devKey).to.equal('new-dev-key-here');
+    });
+  });
+  describe('Given experiment strings, ', function() {
+    it('should convert them into objects', function() {
+      var feature = featureClient.configure({
+        experiments: ['testEx3']
+      });
+
+      expect(feature.config.experiments).to.eql([{ name: 'testEx3', default: false }]);
+    });
+  });
+  describe('Given experiment objects, ', function() {
+    it('should retain names, defaults, and descriptions', function() {
+      var feature = featureClient.configure({
+        experiments: [{ name: 'testEx4', default: true, description: 'Description' }]
+      });
+
+      expect(feature.config.experiments).to.eql([{ name: 'testEx4', default: true, description: 'Description' }]);
+    });
+  });
+
 
 });
