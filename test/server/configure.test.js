b46a4012d36b28114a6474bd6aecccfc9e9af4dc
Dan Crews <crewsd@gmail.com>
2014-10-01 14:19:48 -0600

    Singleton model

--- test/server/configure.test.js
+++ test/server/configure.test.js
@@ -1,87 +1,78 @@
 /*jshint expr: true */
-var chai = require('chai')
-  , expect = require('chai').expect
-  , featureClient = require('../../lib');
-
-
 process.env.FEATURE_URL = 'http://example.com/';
 process.env.FEATURE_DEVKEY = 'exp-dev-key-here';
 process.env.FEATURE_DEVKEY_SHARED = 'shared-exp-dev-key-here';
 
+var chai = require('chai')
+  , expect = require('chai').expect
+  , feature = require('../../lib');
 
 describe('.config interface:', function() {
   describe('Given no config object,', function() {
     it('should reject with an Error', function() {
       expect(function() {
-        featureClient.configure();
-      }).to.throw(Error, 'Cannot register experiments without a config. Please see docs');
+        feature.configure();
+      }).to.throw(Error, 'Cannot register experiments without a config. Please see the docs');
     });
   });
   describe('Given no experiments array', function() {
     it('should reject with an Error', function() {
       expect(function() {
-        featureClient.configure({});
+        feature.configure({});
       }).to.throw(Error, 'Cannot register experiments without `experiments`. Please see the docs');
     });
   });
   describe('Given no experiments in array', function() {
     it('should reject with an Error', function() {
       expect(function() {
-        featureClient.configure({
+        feature.configure({
           experiments : []
         });
       }).to.throw(Error, 'Cannot register experiments without `experiments`. Please see the docs');
     });
   });
   describe('Given no optional Configurations', function() {
-    it('should fall back to `process.env`', function() {
-      var feature = featureClient.configure({
+    it('should fall back to `process.env` and defaults', function() {
+      feature.configure({
         experiments: ['testEx'],
         shared: { experiments: [] }
       });
 
-      expect(feature.featureUrl).to.equal('http://example.com/');
-      expect(feature.devKey).to.equal('exp-dev-key-here');
-      expect(feature.sharedDevKey).to.equal('shared-exp-dev-key-here');
+      expect(feature._settings.featureUrl).to.equal('http://example.com/');
+      expect(feature._settings.devKey).to.equal('exp-dev-key-here');
+      expect(feature._settings.sharedDevKey).to.equal('shared-exp-dev-key-here');
+
+      expect(feature._settings.timeout).to.equal(5000);
     });
   });
   describe('Given a passed devKey / featureUrl,', function() {
     it('should not fallback to `process.env`', function() {
-      var feature = featureClient.configure({
+      feature.configure({
         featureUrl: 'https://example.com/',
         devKey: 'new-dev-key-here',
         experiments: ['testEx2']
       });
 
-      expect(feature.featureUrl).to.equal('https://example.com/');
-      expect(feature.devKey).to.equal('new-dev-key-here');
+      expect(feature._settings.featureUrl).to.equal('https://example.com/');
+      expect(feature._settings.devKey).to.equal('new-dev-key-here');
     });
   });
   describe('Given experiment strings, ', function() {
     it('should convert them into objects', function() {
-      var feature = featureClient.configure({
+      feature.configure({
         experiments: ['testEx3']
       });
 
-      expect(feature.config.experiments).to.eql([{ name: 'testEx3', default: false }]);
+      expect(feature._appConfig.experiments).to.eql([{ name: 'testEx3', default: false }]);
     });
   });
   describe('Given experiment objects, ', function() {
     it('should retain names, defaults, and descriptions', function() {
-      var feature = featureClient.configure({
+      feature.configure({
         experiments: [{ name: 'testEx4', default: true, description: 'Description' }]
       });
 
-      expect(feature.config.experiments).to.eql([{ name: 'testEx4', default: true, description: 'Description' }]);
-    });
-  });
-  describe('Given no timeout, ', function() {
-    it('should set timeout to be 5s', function() {
-      var feature = featureClient.configure({
-        experiments: ['test']
-      });
-
-      expect(feature.timeout).to.eql(5000);
+      expect(feature._appConfig.experiments).to.eql([{ name: 'testEx4', default: true, description: 'Description' }]);
     });
   });
 
